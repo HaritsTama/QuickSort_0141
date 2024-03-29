ac9ba2e1504d69fa53ed748e8f80508f3c5fa953
HaritsTama <nayotama1818@gmail.com>
2024-03-29 09:46:34 +0700

    menuliskan step 2, 3, dan 4 juga menambahkan proses perulangan while di dalam prosedur q_short

--- QuickSort_0141/QuickSort_0141.cpp
+++ QuickSort_0141/QuickSort_0141.cpp
@@ -31,6 +31,7 @@ void input() {
 //Menukar element pada index x dengan element pada index y
 void swap(int x, int y)
 {
+
 	int temp = arr[x];
 	arr[x] = arr[y];
 	arr[y] = temp;
@@ -46,4 +47,9 @@ void q_short(int low, int high);
 		return;
 	}
 		
+	pivot = arr[low]; //step 2
+	i = low + 1; //step 3
+	j = high; // step 4
+
+	while (i <= j) //step 10
 }
