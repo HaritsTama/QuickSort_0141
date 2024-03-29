049c04e22be9764eb27cf7a63b9ce1477fbac49d
HaritsTama <nayotama1818@gmail.com>
2024-03-29 10:18:46 +0700

    menambahkan proses perulangan for didalam prosedur display dan menyelesaikan prosedur display

--- QuickSort_0141/QuickSort_0141.cpp
+++ QuickSort_0141/QuickSort_0141.cpp
@@ -89,5 +89,16 @@ void q_short(int low, int high)
 
 void display()
 {
-	cout << "\n"
+	cout << "\n-----------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n\nJumlah Perbadingan : " << cmp_count << endl;
+	cout << "Jumlah Pergerakan Data : " << mov_count << endl;
 }
+
+
