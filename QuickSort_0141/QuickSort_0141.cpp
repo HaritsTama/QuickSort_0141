265183e51508978b2d508ded5bcd8b724d0d1f46
HaritsTama <nayotama1818@gmail.com>
2024-03-29 09:35:19 +0700

    menyelesaikan prosedur swap dan mendeklarasikan variable lokal

--- QuickSort_0141/QuickSort_0141.cpp
+++ QuickSort_0141/QuickSort_0141.cpp
@@ -31,6 +31,9 @@ void input() {
 //Menukar element pada index x dengan element pada index y
 void swap(int x, int y)
 {
-	
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
 
 }
